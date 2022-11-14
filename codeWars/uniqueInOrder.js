b702eca7b7a18510a8d8aad2b813f21ff233228a
Joseph Cordell <joseph@jcordell.com>
2022-11-14 09:46:43 -0800

    UniqueInOrder

---
+++ codeWars/uniqueInOrder.js
@@ -0,0 +1,19 @@
+/* Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3] */
+
+var uniqueInOrder = function (it) {
+    let answer = []
+    for (let i = 0; i < it.length; i++) {
+        if (it[i] !== it[i+1]) answer.push(it[i])
+    }
+    return answer
+}
+
+let bit = [1,2,2,3,3]
+
+console.log(uniqueInOrder(bit));
