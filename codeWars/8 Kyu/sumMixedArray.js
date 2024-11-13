879373f6ea26f528f6c4005ca37e011b1d77aa0d
Joseph Cordell <joseph@jcordell.com>
2024-11-12 23:17:15 -0800

    SumMixedArray

---
+++ codeWars/8 Kyu/sumMixedArray.js
@@ -0,0 +1,15 @@
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+sumMix = (x) => {
+    answer = 0
+    for (let i = 0; i < x.length; i++) {
+        answer += Number(x[i])
+    }
+    return answer
+}
+
+console.log(sumMix([9, 3, '7', '3']))
+
+// return x.map(a => +a).reduce((a, b) => a + b);
